0d125efeb1daed71696a2269a12fd01e4063c068
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-04-11 16:39:45 -0500

    working on features

--- src/components/landingPage/LandingLayout.js
+++ src/components/landingPage/LandingLayout.js
@@ -8,6 +8,7 @@ export const LandingLayout = styled.div`
     display: flex;
     margin: 4rem 0;
     position: relative;
+    min-height: 55vh;
 
     &::before {
       position: absolute;
@@ -16,7 +17,7 @@ export const LandingLayout = styled.div`
       left: 0;
       height: 200px;
       width: 100%;
-      background-color: var(--secondary);
+      background-color: var(--primary);
       border-radius: 1rem;
     }
 
@@ -24,11 +25,25 @@ export const LandingLayout = styled.div`
       margin: 20px;
       flex: 0 0 30%;
       z-index: 1;
+      position: relative;
 
       img {
         width: 100%;
         border: 10px solid #fff;
-        border-radius: 1rem;
+        //   border-radius: 1rem;
+        border-radius: 50%;
+      }
+
+      ::after {
+        content: "";
+        background: var(--primary);
+        height: 20rem;
+        width: 100%;
+        position: absolute;
+        bottom: 3.3rem;
+        left: 0;
+        border: 10px solid #fff;
+        z-index: -1;
       }
     }
 
@@ -37,41 +52,94 @@ export const LandingLayout = styled.div`
       font-size: 3rem;
       margin: 20px;
       flex: 1;
-    }
 
-    h1 {
-      color: var(--white);
-      font-weight: 700;
-      margin-bottom: 2rem;
-    }
+      h1 {
+        color: var(--white);
+        font-weight: 700;
+        margin-bottom: 2rem;
+      }
 
-    .description {
-      font-size: 2rem;
-      margin-top: 4rem;
-      p {
-        margin: 1rem 0;
+      .description {
+        font-size: 2rem;
+        margin-top: 4rem;
+        p {
+          margin: 1rem 0;
+        }
       }
     }
   }
 
-  .expect {
-    border: 4px solid blue;
+  .tech-stack {
     margin-bottom: 4rem;
 
     &-title {
-      margin: 2rem 0;
+      font-size: 3rem;
+      font-style: italic;
+      font-weight: bold;
+      text-transform: uppercase;
+      margin: 2rem;
+      text-align: center;
     }
 
     &-cards {
-      display: flex;
-      justify-content: space-between;
-      margin: 2rem 0;
+      display: grid;
+      grid-column: 1/-1;
+      -ms-grid-columns: (minmax(100px, 1fr)) [auto-fit];
+      grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+      background: #fff;
+      grid-gap: 2rem;
+      padding: 2rem;
+      perspective: 1000px;
     }
 
     &-card {
-      border: 4px solid cyan;
-      height: 20vh;
-      width: 100%;
+      background: #fff;
+      border-radius: 20px;
+      padding: 1rem;
+      padding-top: 1rem;
+      text-align: center;
+      padding-top: 3rem;
+      position: relative;
+      transform-origin: top center;
+      --rotate: 10deg;
+      transform: rotateX(var(--rotate));
+      transition: transform 0.2s;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+
+      :hover {
+        transform: rotateX(-20deg);
+      }
+
+      img {
+        height: 80px;
+        width: 100%;
+        -o-object-fit: contain;
+        object-fit: contain;
+      }
+
+      p {
+        font-size: 1.7rem;
+        font-weight: 700;
+        margin: 1rem 0;
+      }
+
+      &::before,
+      &::after {
+        content: "";
+        display: block;
+        width: 8px;
+        height: 8px;
+        background: var(--primary);
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.1);
+        top: 15px;
+        position: absolute;
+      }
+      &::after {
+        right: 20px;
+      }
+      &::before {
+        left: 20px;
+      }
     }
   }
 
@@ -79,7 +147,6 @@ export const LandingLayout = styled.div`
     border: 4px solid blue;
     padding: 2rem;
     margin-bottom: 4rem;
-    min-height: 60vh;
 
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -88,6 +155,31 @@ export const LandingLayout = styled.div`
 
     &-card {
       border: 4px solid green;
+      display: flex;
+
+      &-container {
+        display: flex;
+        flex-direction: column;
+
+        h4 {
+          font-size: 2.5rem;
+          font-weight: bold;
+          font-style: italic;
+          text-transform: uppercase;
+          margin-bottom: 2rem;
+        }
+      }
+
+      &-image {
+        width: 20rem;
+        height: 15rem;
+
+        img {
+          max-width: 100%;
+          max-height: 100%;
+          border: 1px solid grey;
+        }
+      }
     }
   }
 `;
--- src/components/landingPage/LandingPage.js
+++ src/components/landingPage/LandingPage.js
@@ -1,7 +1,14 @@
 import React from "react";
-import { Link } from "gatsby";
 import { LandingLayout } from "./LandingLayout";
 import profileImage from "../../images/profileImage.jpg";
+import reactImage from "../../images/logo-react.svg";
+import angularImage from "../../images/logo-angular.png";
+import typeScriptImage from "../../images/logo-typescript.png";
+import vueImage from "../../images/logo-vue.png";
+import vanilaImage from "../../images/logo-vanila.png";
+import nodeImage from "../../images/logo-node.png";
+import youtubeImage from "../../images/youtube-img.png";
+import blogImage from "../../images/blog-img.jpg";
 
 const LandingPage = ({ site }) => {
   const { title, description, descriptionSub } = site.siteMetadata;
@@ -19,34 +26,65 @@ const LandingPage = ({ site }) => {
           </div>
         </div>
       </section>
-      <section className="expect">
-        <div className="expect-title">Lorem ipsum dolor sit.</div>
-        <div className="expect-cards">
-          <div className="expect-card">card 1</div>
-          <div className="expect-card">card 2</div>
-          <div className="expect-card">card 3</div>
-          <div className="expect-card">card 4</div>
-          <div className="expect-card">card 5</div>
-          <div className="expect-card">card 6</div>
+      <section className="tech-stack">
+        <h4 className="tech-stack-title">Learn more about these techs</h4>
+        <div className="tech-stack-cards">
+          <div className="tech-stack-card">
+            <img src={reactImage} alt="React Framework" />
+            <p>React</p>
+          </div>
+          <div className="tech-stack-card">
+            <img src={vueImage} alt="Vue Framework" />
+            <p>Vue</p>
+          </div>
+          <div className="tech-stack-card">
+            <img src={angularImage} alt="" />
+            <p>Angular</p>
+          </div>
+          <div className="tech-stack-card">
+            <img src={typeScriptImage} alt="Typescript" />
+            <p>Typescript</p>
+          </div>
+          <div className="tech-stack-card">
+            <img src={nodeImage} alt="NodeJS" />
+            <p>Node</p>
+          </div>
+          <div className="tech-stack-card">
+            <img src={vanilaImage} alt="Vanila Javascript" />
+            <p>Vanila</p>
+          </div>
         </div>
       </section>
       <section className="features">
-        <div className="features-card">card 1</div>
-        <div className="features-card">card 2</div>
+        <div className="features-card">
+          <div className="features-card-container">
+            <h4>Youtube</h4>
+            <div className="features-card-image">
+              <img src={youtubeImage} alt="Youtube" />
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            sapiente error quia reiciendis! Voluptatibus saepe ipsam sapiente
+            architecto numquam soluta?
+          </p>
+        </div>
+        <div className="features-card">
+          <div className="features-card-container">
+            <h4>Blog</h4>
+            <div className="features-card-image">
+              <img src={blogImage} alt="Youtube" />
+            </div>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+            sapiente error quia reiciendis! Voluptatibus saepe ipsam sapiente
+            architecto numquam soluta?
+          </p>
+        </div>
         <div className="features-card">card 3</div>
         <div className="features-card">card 4</div>
       </section>
-      {/* <div className="landing-img">
-        <img src={profileImage} alt={site.siteMetadata.author} />
-      </div>
-      <h1 className="landing-title">{site.siteMetadata.title}</h1>
-      <div className="landing-text">
-        <p>{site.siteMetadata.description}</p>
-        <p>{site.siteMetadata.descriptionSub}</p>
-        <p>
-          Need a developer ? <Link to="/contact">Contact me.</Link>
-        </p>
-      </div> */}
     </LandingLayout>
   );
 };
