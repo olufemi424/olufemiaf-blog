382261872ddde72f1dff0fd89fed75cfdcdadfa8
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-04-18 20:12:31 -0500

    posts page

--- src/components/postList/PostItemStyles.js
+++ src/components/postList/PostItemStyles.js
@@ -1,22 +1,30 @@
 import styled from "styled-components";
 
 export const PostListItemCard = styled.div`
-  padding: 3rem 2.5rem;
-  margin: 3rem 0;
-  border-left: 5px solid var(--primary);
-  background-color: var(--white);
-  border-radius: 0 1rem 1rem 0;
-  box-shadow: 0 0 1px 1px rgba(20,23,28,.1), 0 3px 1px 0 rgba(20,23,28,.1);
-}
+  flex-basis: 50%;
+  max-width: 45%;
 
-  :hover{
+  :nth-of-type(3n) {
+    flex-basis: 100%;
+  }
+
+  padding: 3rem 2.5rem;
+  margin: auto;
+  margin-bottom: 3rem;
+  background-color: var(--white);
+  border-radius: 1rem;
+  box-shadow: 0 0 1px 1px rgba(20, 23, 28, 0.1),
+    0 3px 1px 0 rgba(20, 23, 28, 0.1);
+  transition: 300ms ease-in;
+
+  :hover {
     background-color: grey;
     color: white;
   }
 
   a {
-    text-decoration:none;
-    color:var(--black);
+    text-decoration: none;
+    color: var(--black);
   }
 
   h1 {
@@ -27,12 +35,12 @@ export const PostListItemCard = styled.div`
   .post-details-group {
     display: flex;
     align-items: center;
-    margin-bottom:2rem;
+    margin-bottom: 2rem;
   }
 
   .post-author {
     font-size: 1.5rem;
-    margin-right:2rem;
+    margin-right: 2rem;
   }
 
   .post-date {
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby";
 import styled from "styled-components";
 import Layout from "../layouts/Layout";
 import Head from "../components/head/Head";
-// import PostList from "../components/postList/PostListItem";
+import PostList from "../components/postList/PostListItem";
 import { H1 } from "../styles/CommonStyles";
 
 const BlogPage = ({ data }) => {
@@ -13,10 +13,12 @@ const BlogPage = ({ data }) => {
     <Layout>
       <Head title="Blog" />
       <H1>Blog Posts</H1>
-      {data.allMarkdownRemark &&
-        data.allMarkdownRemark.edges.map((post, i) => (
-          <PostList key={i} post={post}></PostList>
-        ))}
+      <BlogListLayoutStule>
+        {data.allMarkdownRemark &&
+          data.allMarkdownRemark.edges.map((post, i) => (
+            <PostList key={i} post={post}></PostList>
+          ))}
+      </BlogListLayoutStule>
     </Layout>
   );
 };
@@ -41,74 +43,11 @@ export const pageQuery = graphql`
   }
 `;
 
-// Post list
-const PostListItemCard = styled.div`
-  padding: 3rem 2.5rem;
-  margin: 3rem 0;
-  border-left: 5px solid var(--primary);
-  background-color: var(--white);
-  border-radius: 0 1rem 1rem 0;
-  box-shadow: 0 0 1px 1px rgba(20,23,28,.1), 0 3px 1px 0 rgba(20,23,28,.1);
-}
-
-  :hover{
-    background-color: grey;
-    color: white;
-  }
-
-  a {
-    text-decoration:none;
-    color:var(--black);
-  }
-
-  h1 {
-    font-size: 2.4rem;
-    margin-bottom: 2rem;
-  }
-
-  .post-details-group {
-    display: flex;
-    align-items: center;
-    margin-bottom:2rem;
-  }
-
-  .post-author {
-    font-size: 1.5rem;
-    margin-right:2rem;
-  }
-
-  .post-date {
-    font-size: 1.4rem;
-  }
-
-  .post-excerpt {
-    font-size: 1.5rem;
-  }
+const BlogListLayoutStule = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
 `;
-
-const PostList = ({ post }) => {
-  return (
-    <PostListItemCard>
-      <Link to={`/blog/${post.node.fields.slug}`}>
-        <h1>{post.node.frontmatter.title}</h1>
-        <div className="post-details-group">
-          <p className="post-author">
-            <span role="img" aria-label="creator">
-              📝
-            </span>{" "}
-            {post.node.frontmatter.author}
-          </p>
-          <p className="post-date">
-            <span role="img" aria-label="date">
-              📆
-            </span>
-            {post.node.frontmatter.date}
-          </p>
-        </div>
-        <p className="post-excerpt">{post.node.excerpt}</p>
-      </Link>
-    </PostListItemCard>
-  );
-};
 
 export default BlogPage;
