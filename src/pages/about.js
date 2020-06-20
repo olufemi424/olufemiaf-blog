e21ab36cdbc9786d07a5d6b074bfaed27fc255a6
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-06-19 22:43:34 -0500

    prepare for fresh landing page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,14 +2,16 @@ import React from "react";
 import { graphql } from "gatsby";
 
 import Layout from "../layouts/Layout";
+import LandingPage from "../components/about/About";
 import Head from "../components/head/Head";
 
-const Index = ({ data }) => {
+const About = ({ data }) => {
   const { site } = data;
 
   return (
     <Layout>
       <Head title="Home" />
+      <LandingPage site={site}></LandingPage>
     </Layout>
   );
 };
@@ -26,4 +28,4 @@ export const pageQuery = graphql`
   }
 `;
 
-export default Index;
+export default About;
