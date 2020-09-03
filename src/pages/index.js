341b6fe7f33a5e799a6cf0c9df2f351882644bb7
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-09-02 21:29:45 -0500

    comment

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,3 +31,5 @@ export const pageQuery = graphql`
 `;
 
 export default Index;
+
+//  Need to draw out whats next and moving forward with this application, remove this comment when you make significant progress. 
